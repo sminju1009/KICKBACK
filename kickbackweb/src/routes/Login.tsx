3fcbce6991a08d9337ec74b96445033b5db9fefc
김영일 <sugo2753@gmail.com>
2024-04-29 15:19:02 +0900

    Feat[Web]: 로그인, 회원가입 페이지 구현

--- kickbackweb/src/routes/Login.tsx
+++ kickbackweb/src/routes/Login.tsx
@@ -1,12 +1,28 @@
-import React from 'react'
-import { LoginBackBox } from '../styles/User/Login';
-import LoginBack from "../assets/loginBack.png"
+import React, { useState } from 'react'
+import { LogoBox } from '../styles/User/Login';
+import Logo from "../assets/logo3.png"
+import LoginCom from '../components/User/LoginCom';
+import SignUpCom from '../components/User/SignUpCom';
+import { LoginBackBox } from '../styles/User/Login'
+import { useNavigate } from 'react-router';
 
 const Login = () => {
+  const [doLogin,setDoLogin] = useState(true);
+  const navigate = useNavigate();
+
   return (
-    <LoginBackBox>
-      
-    </LoginBackBox>
+    <>
+      <LogoBox>
+        <img src={Logo} alt="해위" onClick={() => navigate("/")} style={{ cursor:"pointer" }}/>
+      </LogoBox>
+      <LoginBackBox>
+        <div className='select'>
+          <div className='text' style={{ backgroundColor: doLogin ? "#0278f6": "#393939", borderRadius: "5px", color: doLogin ? "white":"#bbbbbb" }} onClick={() => setDoLogin(true)}>KICKBACK 로그인</div>
+          <div className='text' style={{ backgroundColor: !doLogin ? "#0278f6": "#393939", borderRadius: "5px", color: !doLogin ? "white":"#bbbbbb" }} onClick={() => setDoLogin(false)}>KICKBACK 회원가입</div>
+        </div>
+        {doLogin ? <LoginCom />:<SignUpCom />}
+      </LoginBackBox>
+    </>
   )
 }
 
---
+++ kickbackweb/src/stores/AuthStore.tsx
@@ -0,0 +1,35 @@
+import { create } from "zustand";
+import { persist } from "zustand/middleware";
+import {Cookies} from 'react-cookie';
+
+const cookie = new Cookies();
+
+interface AuthStore {
+  PATH: String;
+  isLogin: boolean;
+  login: () => void;
+  logout: () => void;
+}
+
+const useAuthStore = create(
+  persist<AuthStore>(
+    (set, get) => ({
+      PATH: "http://localhost",
+      isLogin: false,
+      tokenExpireTime: null,
+
+      login: () => {
+        set({ isLogin: true });
+      },
+      logout: () => {
+        set({ isLogin: false });
+        cookie.remove("")
+      },
+    }),
+    {
+      name: "userLoginStatus",
+    }
+  )
+);
+
+export default useAuthStore;
