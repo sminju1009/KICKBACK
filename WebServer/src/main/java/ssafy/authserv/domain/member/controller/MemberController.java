a98f0f3598950abfaccc5fedc838e012d42c9e76
조현호 <chh5680@naver.com>
2024-04-29 17:55:37 +0900

    Feat[Auth]: 쿠키 통해서 인가 확인 가능

--- WebServer/src/main/java/ssafy/authserv/domain/member/controller/MemberController.java
+++ WebServer/src/main/java/ssafy/authserv/domain/member/controller/MemberController.java
@@ -77,14 +77,14 @@ public class MemberController {
 //        // JWT 토큰을 쿠키에 저장
         Cookie accessTokenCookie = new Cookie("accessToken", loginResponse.jwtToken().accessToken());
         accessTokenCookie.setPath("/");
-        accessTokenCookie.setMaxAge(25200); // 4200분(25200초)으로 설정 (25200)
+        accessTokenCookie.setMaxAge(7 * 24 * 60 * 60); // 4200분(25200초)으로 설정 (25200)
         accessTokenCookie.setHttpOnly(true); // JavaScript를 통한 접근 방지
 //        accessTokenCookie.setSecure(true); // HTTPS를 통해서만 쿠키 전송
         response.addCookie(accessTokenCookie);
 
         Cookie refreshTokenCookie = new Cookie("refreshToken", loginResponse.jwtToken().refreshToken());
         refreshTokenCookie.setPath("/");
-        refreshTokenCookie.setMaxAge(604800); // 1주일
+        refreshTokenCookie.setMaxAge(14 * 24 * 60 * 60); // 1주일
         refreshTokenCookie.setHttpOnly(true);
 //        refreshTokenCookie.setSecure(true);
         response.addCookie(refreshTokenCookie);
