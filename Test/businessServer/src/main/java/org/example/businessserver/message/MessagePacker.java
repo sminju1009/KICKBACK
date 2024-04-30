910bd1fe1a183d387ba9c955459255dd4b454389
장동재 <djjang0925@gmail.com>
2024-04-30 10:17:22 +0900

    Feat[Live]: success send and receive packet

--- Test/businessServer/src/main/java/org/example/businessserver/message/MessagePacker.java
+++ Test/businessServer/src/main/java/org/example/businessserver/message/MessagePacker.java
@@ -6,23 +6,42 @@ import org.msgpack.core.MessagePack;
 import java.io.IOException;
 
 public class MessagePacker {
-    public static byte[] packing(String message) throws IOException {
+    public static byte[] packing(int command, String message) throws IOException {
+        // MessageBufferPacker를 사용하여 데이터를 패킹합니다.
         MessageBufferPacker packer = MessagePack.newDefaultBufferPacker();
 
         try {
-            int temp = Type.CREATE.getType();
-            System.out.println(temp);
 
-//            packer.packInt(temp);
-
+            // CUnit 객체를 MsgPack 형식으로 패킹합니다.
+            // 패킹 순서는 command, message 순입니다.
+            packer.packArrayHeader(2);
+            packer.packInt(command);
             packer.packString(message);
 
-            byte[] send = packer.toByteArray();
-
-            return send;
+            // 패킹된 데이터를 바이트 배열로 반환합니다.
+            return packer.toByteArray();
         } finally {
             System.out.println("send complete");
             packer.close();
         }
+    }
+}
+
+class CUnit {
+
+    private int command;
+    private String message;
+
+    public CUnit(int command, String message) {
+        this.command = command;
+        this.message = message;
+    }
+
+    public int getCommand() {
+        return command;
+    }
+
+    public String getMessage() {
+        return message;
     }
 }
--- Test/businessServer/src/main/java/org/example/businessserver/service/MessageHandler.java
+++ Test/businessServer/src/main/java/org/example/businessserver/service/MessageHandler.java
@@ -27,12 +27,8 @@ public class MessageHandler {
 //                    }
 
                     try {
-//                        out.sendByteArray(Mono.just(MessagePacker.packing("한국어 테스트"))).then().subscribe();
-//                        out.sendByteArray(Mono.just(MessagePacker.packing("english test"))).then().subscribe();
-//                        out.sendByteArray(Mono.just(MessagePacker.packing("command: create"))).then().subscribe();
-//                        out.sendByteArray(Mono.just(MessagePacker.packing("command: create"))).then().subscribe();
-                        out.sendByteArray(Mono.just(MessagePacker.packing("command: create"))).then().subscribe();
-//                        out.sendByteArray(Mono.just(MessagePacker.packing("test"))).then().subscribe();
+                        out.sendByteArray(Mono.just(MessagePacker.packing(0, "test"))).then().subscribe();
+                        out.sendByteArray(Mono.just(MessagePacker.packing(1, "test"))).then().subscribe();
                     } catch (IOException e) {
                         throw new RuntimeException(e);
                     }
