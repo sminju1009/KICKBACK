0559f9eea3190c22adb9b54ef9446340e5552c09
조현호 <chh5680@naver.com>
2024-05-07 08:54:22 +0900

    Feat[Auth]: 테스트 프로파일 생성

--- WebServer/src/main/java/ssafy/authserv/global/config/JasyptConfig.java
+++ WebServer/src/main/java/ssafy/authserv/global/config/JasyptConfig.java
@@ -9,7 +9,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 @Configuration
-@Profile({"prod"})
+@Profile({"prod", "test"})
 public class JasyptConfig {
 
     @Value("${jasypt.encryptor.key}")
