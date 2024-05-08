2c3d97bc6bb50bbf66bc255d8dcb9f1b4dcd7a23
조현호 <chh5680@naver.com>
2024-05-08 10:02:40 +0900

    Feat[auth]: 토큰 만료 시 에러 처리

--- WebServer/src/main/java/ssafy/authserv/global/jwt/exception/JwtErrorCode.java
+++ WebServer/src/main/java/ssafy/authserv/global/jwt/exception/JwtErrorCode.java
@@ -7,9 +7,9 @@ import org.springframework.http.HttpStatus;
 @Getter
 @AllArgsConstructor
 public enum JwtErrorCode {
-    EXPIRED_TOKEN(HttpStatus.UNAUTHORIZED, "토큰이 만료돼쓰" ),
-    INVALID_TOKEN(HttpStatus.BAD_REQUEST, "사용할 수 없는 토큰쓰"),
-    SIGNATURE_INVALID(HttpStatus.FORBIDDEN, "토큰의 서명 검증에 실패");
+    EXPIRED_TOKEN(HttpStatus.UNAUTHORIZED, "토큰이 만료되었습니다." ),
+    INVALID_TOKEN(HttpStatus.BAD_REQUEST, "사용할 수 없는 토큰입니다."),
+    SIGNATURE_INVALID(HttpStatus.FORBIDDEN, "토큰의 서명 검증에 실패했습니다.");
 
     private final HttpStatus httpStatus; // 에러 상황에 해당하는 HTTP 상태
     private final String errorMessage; // 에러 상황을 설명하는 메시지
