e1abc5a42c6754cc826978eddf75ea5f2da27068
김영일 <sugo2753@gmail.com>
2024-05-09 16:56:21 +0900

    Feat[Business]: 비즈니스 서버 type 별 로직 구현

---
+++ businessServer/src/main/java/org/example/businessserver/object/Rooms.java
@@ -0,0 +1,10 @@
+package org.example.businessserver.object;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Room {
+    private static final List<String> GameRoomList = new ArrayList<>();
+
+
+}
