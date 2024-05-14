46d6443ae924012c6f430145f642c3bd6a103093
장동재 <djjang0925@gmail.com>
2024-05-14 10:59:21 +0900

    Feat[Web]: 게시글 삭제

--- kickbackweb/src/styles/Community/Article/Create.tsx
+++ kickbackweb/src/styles/Community/Article/Create.tsx
@@ -8,7 +8,6 @@ export const Background = styled.div`
   width: 100vw;
   height: 100vh;
   background-color: rgba(0, 0, 0, 0.2);
-  
 `
 
 export const Wrapper = styled.div`
---
+++ kickbackweb/src/styles/Community/Article/Delete.tsx
@@ -0,0 +1,60 @@
+import styled from "styled-components";
+
+export const Background = styled.div`
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 100vw;
+  height: 100vh;
+  background-color: rgba(0, 0, 0, 0.2);
+`;
+
+export const Wrapper = styled.div`
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 300px;
+  height: 150px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  border-radius: 10px;
+  box-shadow: 4px 4px 4px rgba(0, 0, 0, 0.3);
+  background-color: rgba(255, 255, 255, 0.9);
+
+  .topBox {
+    width: 100%;
+    text-align: center;
+    font-size: 18px;
+  }
+
+  .bottomBox {
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+  }
+`;
+
+export const Btn = styled.button`
+  width: 70px;
+  height: 30px;
+  border: none;
+  border-radius: 10px;
+  transition: all 0.2s;
+
+  &:hover {
+    transition: all 0.2s;
+    transform: scale(1.1);
+  }
+
+  &.delete {
+    background-color: rgba(255, 0, 0, 0.75);
+  }
+
+  &.cancle {
+    background-color: gray;
+  }
+`;
